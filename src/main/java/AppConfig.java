0e07c311f2c421be3150d51409b9128f9b1ea413
Ctfoee <hs.wks@mail.ru>
2023-05-28 15:02:09 +0300

    com

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,14 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+
+    @Bean(name="cat")
+    @Scope(value="prototype")
+    public Cat getCat() {
+        Cat cat = new Cat();
+        cat.setName("Murzik");
+        cat.setBreed("Yard");
+        cat.setAge(6);
+        return cat;
+    }
 }
